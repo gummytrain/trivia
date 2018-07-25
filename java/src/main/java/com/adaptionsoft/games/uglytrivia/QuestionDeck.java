bf63a11203b527f32d52cfc854303e18b323d270
Gabriele Cipriano <gabri.cipriano@gmail.com>
2018-07-25 16:09:41 +0100

    moved fillQuestions method

--- java/src/main/java/com/adaptionsoft/games/uglytrivia/QuestionDeck.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/QuestionDeck.java
@@ -1,4 +1,16 @@
 package com.adaptionsoft.games.uglytrivia;
 
 public class QuestionDeck {
+    public String createRockQuestion(int index){
+        return "Rock Question " + index;
+    }
+
+    public void fillQuestions(Game game) {
+        for (int i = 0; i < 50; i++) {
+            game.popQuestions.addLast("Pop Question " + i);
+            game.scienceQuestions.addLast(("Science Question " + i));
+            game.sportsQuestions.addLast(("Sports Question " + i));
+            game.rockQuestions.addLast(createRockQuestion(i));
+        }
+    }
 }
