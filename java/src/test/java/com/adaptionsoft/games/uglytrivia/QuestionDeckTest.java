d7183a68df883877f2729959fcc989ea1f7b7be7
Gabriele Cipriano <gabri.cipriano@gmail.com>
2018-07-25 17:07:24 +0100

    covered other categories

--- java/src/test/java/com/adaptionsoft/games/uglytrivia/QuestionDeckTest.java
+++ java/src/test/java/com/adaptionsoft/games/uglytrivia/QuestionDeckTest.java
@@ -9,12 +9,16 @@ import static org.hamcrest.Matchers.is;
 class QuestionDeckTest {
 
     @ParameterizedTest
-    @CsvSource({"0, Pop", "4, Pop", "8, Pop"})
-    void position_zero_is_a_pop_category(int playerPosition, String expectedCategoy) {
+    @CsvSource({
+            "0, Pop", "4, Pop", "8, Pop",
+            "1, Science", "5, Science", "9, Science",
+            "2, Sports", "6, Sports", "10, Sports"
+    })
+    void placed_categories(int playerPosition, String expectedCategory) {
         QuestionDeck questionDeck = new QuestionDeck();
 
         String actualCategory = questionDeck.currentCategoryFor(playerPosition);
 
-        assertThat(actualCategory, is(expectedCategoy));
+        assertThat(actualCategory, is(expectedCategory));
     }
 }
