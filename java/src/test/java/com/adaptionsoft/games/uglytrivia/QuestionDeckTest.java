a24aa04f0cdbfb1cb3adc853de95b48f90fbb664
Gabriele Cipriano <gabri.cipriano@gmail.com>
2018-07-25 17:02:55 +0100

    expected category is now a parameter

--- java/src/test/java/com/adaptionsoft/games/uglytrivia/QuestionDeckTest.java
+++ java/src/test/java/com/adaptionsoft/games/uglytrivia/QuestionDeckTest.java
@@ -1,7 +1,7 @@
 package com.adaptionsoft.games.uglytrivia;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -9,12 +9,12 @@ import static org.hamcrest.Matchers.is;
 class QuestionDeckTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {"0", "4", "8"})
-    void position_zero_is_a_pop_category(int playerPosition) {
+    @CsvSource({"0, Pop", "4, Pop", "8, Pop"})
+    void position_zero_is_a_pop_category(int playerPosition, String expectedCategoy) {
         QuestionDeck questionDeck = new QuestionDeck();
 
         String actualCategory = questionDeck.currentCategoryFor(playerPosition);
 
-        assertThat(actualCategory, is("Pop"));
+        assertThat(actualCategory, is(expectedCategoy));
     }
 }
