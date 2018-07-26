0e23eaa159898c1cebd5221df07830403c57e9af
Gabriele Cipriano <gabri.cipriano@gmail.com>
2018-07-26 10:01:39 +0100

    rock is placed out of the board

--- java/src/test/java/com/adaptionsoft/games/uglytrivia/QuestionDeckTest.java
+++ java/src/test/java/com/adaptionsoft/games/uglytrivia/QuestionDeckTest.java
@@ -1,5 +1,6 @@
 package com.adaptionsoft.games.uglytrivia;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -21,5 +22,14 @@ class QuestionDeckTest {
         String actualCategory = questionDeck.currentCategoryFor(playerPosition);
 
         assertThat(actualCategory, is(expectedCategory));
+    }
+
+    @Test
+    void out_of_the_board() {
+        QuestionDeck questionDeck = new QuestionDeck();
+
+        String actualCategory = questionDeck.currentCategoryFor(12);
+
+        assertThat(actualCategory, is("Rock"));
     }
 }
