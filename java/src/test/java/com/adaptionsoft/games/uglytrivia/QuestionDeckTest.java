cb9353efa168f20d965a6235a732d228d7d5b85a
Gabriele Cipriano <gabri.cipriano@gmail.com>
2018-07-26 09:58:58 +0100

    We can assume that rock is placed at 3/7/11

--- java/src/test/java/com/adaptionsoft/games/uglytrivia/QuestionDeckTest.java
+++ java/src/test/java/com/adaptionsoft/games/uglytrivia/QuestionDeckTest.java
@@ -12,7 +12,8 @@ class QuestionDeckTest {
     @CsvSource({
             "0, Pop", "4, Pop", "8, Pop",
             "1, Science", "5, Science", "9, Science",
-            "2, Sports", "6, Sports", "10, Sports"
+            "2, Sports", "6, Sports", "10, Sports",
+            "3, Rock", "7, Rock", "11, Rock",
     })
     void placed_categories(int playerPosition, String expectedCategory) {
         QuestionDeck questionDeck = new QuestionDeck();
